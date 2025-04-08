375632fc9c11485e9ac27f8b87e86e69c56a999a
agustin-farias-123 <fariasthomas946@gmail.com>
2025-04-08 11:01:49 -0300

    creacion del producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { FormularioComponent } from './contacto/formulario/formulario.component';
+import { ProductoComponent } from './productos/producto/producto.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/inicio',pathMatch:'full'},
     {path:'inicio',component:InicioComponent},
-    {path:'contacto',component:FormularioComponent}
+    {path:'contacto',component:FormularioComponent},
+    {path:'producto',component:ProductoComponent},
 ];
