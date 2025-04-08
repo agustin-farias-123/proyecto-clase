cd9bdf2a770457b43c42d1306e2640e7d7dfcd73
agustin-farias-123 <fariasthomas946@gmail.com>
2025-04-08 09:08:52 -0300

    primer proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyecto_2025';
+}
