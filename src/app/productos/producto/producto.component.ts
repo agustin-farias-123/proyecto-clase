7aa105a5e4f56f16eaa834ccf489662e8cd47612
agustin-farias-123 <fariasthomas946@gmail.com>
2025-04-10 11:02:19 -0300

    directivas

--- src/app/productos/producto/producto.component.ts
+++ src/app/productos/producto/producto.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
+import {  FormGroup, FormsModule, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
+/*
+  productos =  [
+  {nombre: 'Producto 1', precio: 100},
+  {nombre: 'Producto 2', precio: 200},
+  {nombre: 'Producto 3', precio: 300}
+];
 
+usuario = {
+  nombre: 'thomas',
+  activo: true
+};
+*/
+usuario={
+  nombre:''
 }
+}
+
